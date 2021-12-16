8d5d1a4fb40ecf621cfbcf589ddfd004e44c9bc3
ostriandoni <donny.ostrianto@gmail.com>
2021-12-16 20:01:00 +0700

    finishing bmi calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'input_page.dart';
+import 'screens/input_page.dart';
 
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: InputPage(),
-      theme: ThemeData.dark()
-    );
+    return MaterialApp(home: InputPage(), theme: ThemeData.dark());
   }
 }
